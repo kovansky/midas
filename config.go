58ebc673ae29cc226df89df43825a7c2f3f0a71a
kovansky <skowanski@f4dev.me>
2022-02-02 19:17:18 +0100

    Added context and errors

--- config.go
+++ config.go
@@ -2,5 +2,5 @@ package strapi2hugo
 
 type Config struct {
 	ListeningPort int             `json:"listeningPort"`
-	Sites         map[string]Site `json:"sites"` // [api key] => hugo site
+	Sites         map[string]Site `json:"sites"` // [api key] => site
 }
---
+++ context.go
@@ -0,0 +1,23 @@
+package strapi2hugo
+
+import "context"
+
+// contextKey represents an internal key for adding context fields.
+// This practice is considered best as it prevents other packages
+// from interfering with our context keys.
+type contextKey int
+
+const (
+	userConfigContextKey = contextKey(iota + 1)
+)
+
+// NewContextWithUserConfig returns a new context with given config.
+func NewContextWithUserConfig(ctx context.Context, config interface{}) context.Context {
+	return context.WithValue(ctx, userConfigContextKey, config)
+}
+
+// UserConfigFromContext returns current config from context.
+func UserConfigFromContext(ctx context.Context) interface{} {
+	config, _ := ctx.Value(userConfigContextKey).(interface{})
+	return config
+}
