e1539170d5484426d0fafedc2a81a8311180d55a
kovansky <skowanski@f4dev.me>
2022-02-12 04:01:24 +0100

    Add specific styles to sanitizing policies

--- bluemonday/sanitizer.go
+++ bluemonday/sanitizer.go
@@ -21,22 +21,44 @@ func NewSanitizerService() *SanitizerService {
 }
 
 func (s *SanitizerService) injectDefaultPolicy() {
+	textElements := []string{
+		"span", "p",
+	}
+
+	classAllowedElements := append(textElements,
+		"mark",
+		"ol", "ul",
+		"figure")
+
 	p := bluemonday.UGCPolicy()
 
 	// For youtube, instagram etc...
 	p.AllowAttrs("url").OnElements("oembed") // ToDo: allow only some urls
 
-	// Text display control - fonts, sizes, colors
-	p.AllowAttrs("class", "style").OnElements("span", "ol", "ul", "figure") // ToDo: add matcher
+	// Text display control
+	p.AllowStyles("font-family").
+		Matching(regexp.MustCompile("(?i)^[a-z0-9\\-_ ,'\\\"]+$")).
+		OnElements(textElements...)
+
+	p.AllowStyles("color"). // Hex (3 chars, 6 chars), rgb, rgba (number or percentage), hsl are supported
+				Matching(regexp.MustCompile("^(#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}))|(rgb\\( *\\d{1,3}%? *, *\\d{1,3}%? *, *\\d{1,3}%? *\\))|(rgba\\( *\\d{1,3}%? *, *\\d{1,3}%? *, *\\d{1,3}%? *, *((1(\\.0+)?)|(0\\.\\d+)) *\\))|(hsl\\( *\\d{1,3} *, *\\d{1,3}% *, *\\d{1,3}% *\\))$")).
+				OnElements(textElements...)
+
+	p.AllowStyles("list-style-types").
+		MatchingEnum("disc", "circle", "square", "decimal", "decimal-leading-zero",
+			"lower-roman", "upper-roman", "lower-greek", "lower-latin", "upper-latin", "armenian",
+			"georgian", "lower-alpha", "upper-alpha", "none").
+		OnElements("ul", "ol")
+
+	p.AllowAttrs("class").Matching(bluemonday.SpaceSeparatedTokens).OnElements(classAllowedElements...)
 
 	// Allow links opening in new tab
 	p.AllowAttrs("target").
-		Matching(regexp.MustCompile("_blank")).
+		Matching(regexp.MustCompile("^_blank$")).
 		OnElements("a")
 
 	p.AllowRelativeURLs(true)
-
-	p.AllowStyling() // ToDo: use AllowStyles instead
+	p.RequireNoReferrerOnFullyQualifiedLinks(true)
 
 	s.policy = p
 }
