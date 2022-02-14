8465aa108df940795067a488c8543a04595ccb5c
kovansky <skowanski@f4dev.me>
2022-02-14 19:46:55 +0100

    Added tests for sanitizers.
    Modified `width` regex. Allowed `rel` on `<a>`

--- bluemonday/sanitizer.go
+++ bluemonday/sanitizer.go
@@ -20,7 +20,8 @@ func NewSanitizerService() *SanitizerService {
 	return sanitizerService
 }
 
-// injectDefaultPolicy creates a local default policy (extension of bluemonday's default UGCPolicy) and injects it into the SanitizerService.
+// injectDefaultPolicy creates a local default policy (extension of bluemonday's default UGCPolicy)
+// and injects it into the SanitizerService.
 //
 // Warning: support for oembeds is currently dropped, but should be added in future (refer to MDS-18)
 func (s *SanitizerService) injectDefaultPolicy() {
@@ -44,20 +45,24 @@ func (s *SanitizerService) injectDefaultPolicy() {
 				Matching(regexp.MustCompile("^(#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}))|(rgb\\( *\\d{1,3}%? *, *\\d{1,3}%? *, *\\d{1,3}%? *\\))|(rgba\\( *\\d{1,3}%? *, *\\d{1,3}%? *, *\\d{1,3}%? *, *((1(\\.0+)?)|(0\\.\\d+)) *\\))|(hsl\\( *\\d{1,3} *, *\\d{1,3}% *, *\\d{1,3}% *\\))$")).
 				OnElements(textElements...)
 
-	p.AllowStyles("list-style-types").
+	p.AllowStyles("list-style-type").
 		MatchingEnum("disc", "circle", "square", "decimal", "decimal-leading-zero",
 			"lower-roman", "upper-roman", "lower-greek", "lower-latin", "upper-latin", "armenian",
 			"georgian", "lower-alpha", "upper-alpha", "none").
 		OnElements("ul", "ol")
 
 	p.AllowStyles("width").
-		Matching(bluemonday.NumberOrPercent).OnElements("figure")
+		Matching(regexp.MustCompile("^([0-9]+(cm|in|mm|pc|pt|px|q|%|vw|vh|vmin|vmax|em|ex|ch|rem))|auto$")).OnElements("figure")
 
 	p.AllowAttrs("class").Matching(bluemonday.SpaceSeparatedTokens).OnElements(classAllowedElements...)
 
 	// Allow links opening in new tab
 	p.AllowAttrs("target").
 		Matching(regexp.MustCompile("^_blank$")).
+		OnElements("a")
+
+	p.AllowAttrs("rel").
+		Matching(regexp.MustCompile("^(alternate|author|bookmark|external|help|license|next|nofollow|noopener|noreferrer|prev|search|tag)$")).
 		OnElements("a")
 
 	p.AllowRelativeURLs(true)
